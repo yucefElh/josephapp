3e6f87e93d35da198850e39b870c48c827dbe41a
josephelh <youssef.harba2020@gmail.com>
2022-05-19 15:56:07 +0100

    master

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import Product from '../models/productModel.js'
 // Fetch all products
 //Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 2;
+  const pageSize = 10;
   const page = Number(req.query.pageNumber) || 1;
   const keyword =req.query.keyword ? {
     name:{
